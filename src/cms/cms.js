0e67bd65dbe34c093dcb78b8a3dd94ee938af8ec
dbertella <daniele.bertella@gmail.com>
2018-10-03 22:52:11 +0100

    Update home page preveiew

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import HomePagePreview from './preview-templates/HomePagePreview'
 
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('chi-siamo', AboutPagePreview)
+CMS.registerPreviewTemplate('cosa-facciamo', ProductPagePreview)
+CMS.registerPreviewTemplate('home', HomePagePreview)
+CMS.registerPreviewTemplate('news', BlogPostPreview)
