ee7652ef26cc26e84a5676bd3e207853b8946db9
dbertella <daniele.bertella@gmail.com>
2018-10-03 08:45:50 +0100

    Add logo and social

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import instagram from '../img/instagram.svg'
+import facebook from '../img/facebook.svg'
+import logo from '../img/logo.png'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -9,7 +10,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="Cascina Rampina" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
@@ -27,12 +28,22 @@ const Navbar = () => (
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://www.instagram.com/cascinarampina/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <span className="icon">
-            <img src={github} alt="Github" />
+            <img src={instagram} alt="Instagram" />
+          </span>
+        </a>
+        <a
+          className="navbar-item"
+          href="https://www.facebook.com/cascinarampina/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={facebook} alt="Facebook" />
           </span>
         </a>
       </div>
