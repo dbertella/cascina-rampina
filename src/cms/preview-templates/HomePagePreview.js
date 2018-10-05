6ab51f31847007a82c1e326613582febeb90a60a
dbertella <daniele.bertella@gmail.com>
2018-10-05 22:37:40 +0100

    Update home preview

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -18,29 +18,33 @@ const HomePagePreview = ({ entry, getAsset }) => {
       title={entry.getIn(['data', 'title'])}
       heading={entry.getIn(['data', 'heading'])}
       description={entry.getIn(['data', 'description'])}
-      intro={{ blurbs }}
+      intro={{
+        image: entry.getIn(['data', 'image']),
+        link: entry.getIn(['data', 'link']),
+        text: entry.getIn(['data', 'text'])
+      }}
       main={{
         heading: entry.getIn(['data', 'main', 'heading']),
         description: entry.getIn(['data', 'main', 'description']),
         image1: {
           image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image1', 'alt']),
+          alt: entry.getIn(['data', 'main', 'image1', 'alt'])
         },
         image2: {
           image: getAsset(entry.getIn(['data', 'main', 'image2', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image2', 'alt']),
+          alt: entry.getIn(['data', 'main', 'image2', 'alt'])
         },
         image3: {
           image: getAsset(entry.getIn(['data', 'main', 'image3', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image3', 'alt']),
-        },
+          alt: entry.getIn(['data', 'main', 'image3', 'alt'])
+        }
       }}
       fullImage={entry.getIn(['data', 'full_image'])}
       testimonials={testimonials}
       pricing={{
         heading: entry.getIn(['data', 'pricing', 'heading']),
         description: entry.getIn(['data', 'pricing', 'description']),
-        plans: pricingPlans,
+        plans: pricingPlans
       }}
     />
   )
@@ -48,9 +52,9 @@ const HomePagePreview = ({ entry, getAsset }) => {
 
 HomePagePreview.propTypes = {
   entry: PropTypes.shape({
-    getIn: PropTypes.func,
+    getIn: PropTypes.func
   }),
-  getAsset: PropTypes.func,
+  getAsset: PropTypes.func
 }
 
 export default HomePagePreview
